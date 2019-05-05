e8366be2545212e552e0964411aa0b677a62001a
Jesse Clark <squee292darkside@Jesses-MacBook-Air.local>
2019-05-05 15:28:21 -0500

    Updated main.py with a sort option and commented out the code

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-bl
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-
+#creating the columns for my database
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
@@ -20,22 +20,24 @@ class Blog(db.Model):
 
 
 
-
+#default page that loads when first going to the site
 @app.route('/')
 def index():
-    blogs = Blog.query.all()
+    blogs = Blog.query.order_by(Blog.id.desc()).all()
     return render_template('main_blog.html', title = "Build a blog", blogs = blogs)
-
+#main page in the site
 @app.route('/blog')
 def blogpage():
+    #path to open a blog on a seperate page
     blog_id = request.args.get('id')
     if (blog_id):
         blog = Blog.query.get(blog_id)
         return render_template('single_blog.html', blog = blog)
     else:
-        blogs = Blog.query.all()
+        #displaying all created blogs in order of newest to oldest
+        blogs = Blog.query.order_by(Blog.id.desc()).all()
         return render_template('main_blog.html' ,title = "Build a blog", blogs = blogs)
-
+#new blog post page
 @app.route('/newpost', methods = ['POST', 'GET'])
 def newpost():
     if request.method == 'POST':
@@ -45,7 +47,7 @@ def newpost():
         body_error = ''
         error_count = 0
 
-
+        #test to validate if anything was entered and return an error if not
         if title == '':
             title_error = 'Please enter a title'
             error_count += 1
@@ -55,7 +57,7 @@ def newpost():
         if error_count > 0:
             return render_template('new_blog.html', title = 'New Blog Entry', title_error = title_error,
                 body_error = body_error)
-
+        #creating a new blog post
         new_entry = Blog(title, body)
     
         db.session.add(new_entry)
@@ -65,10 +67,6 @@ def newpost():
         
     else:
         return render_template('new_blog.html', title = 'New Blog Entry')
-
-
-
-
 
 if __name__ == '__main__':
     app.run()
